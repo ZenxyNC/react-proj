dfe34cc3193c7bdb74788768f2846d30582fce94
ZenxyNC <timothynathanlucas@gmail.com>
2024-07-05 22:43:11 +0700

    A few changes!

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          You Don't Have To Learn React
         </a>
       </header>
     </div>
